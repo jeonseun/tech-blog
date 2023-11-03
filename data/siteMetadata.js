cec2b36c4dfb334f2e69bdcbf40f09c432595adf
jeonseun <jeonseun1234@gmail.com>
2023-11-03 21:48:59 +0900

    chore: 배포에 따른 siteUrl 변경

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   description: 'Java, Spring, MySQL 등을 공부합니다.',
   language: 'ko',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://tech-blog-eta-eight.vercel.app',
   siteRepo: 'https://github.com/jeonseun/tech-blog',
   siteLogo: '/static/images/logo.png',
   socialBanner: './',
