5fc239260f2b8097b98f48b04ad0e08abce9623d
jeonseun <jeonseun1234@gmail.com>
2023-11-03 21:35:50 +0900

    fix: 애널리틱스 설정 오류로 인한 메타데이터 수정
    - siteMetadata.js내 애널리틱스 설정 주석 제거

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -14,25 +14,25 @@ const siteMetadata = {
   github: 'https://github.com/jeonseun',
   locale: 'ko-KR',
   // 애널리틱스 설정
-  // analytics: {
-  //   // If you want to use an analytics provider you have to add it to the
-  //   // content security policy in the `next.config.js` file.
-  //   // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-  //   umamiAnalytics: {
-  //     // We use an env variable for this site to avoid other users cloning our analytics ID
-  //     umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-  //   },
-  //   // plausibleAnalytics: {
-  //   //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-  //   // },
-  //   // simpleAnalytics: {},
-  //   // posthogAnalytics: {
-  //   //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-  //   // },
-  //   // googleAnalytics: {
-  //   //   googleAnalyticsId: '', // e.g. G-XXXXXXX
-  //   // },
-  // },
+  analytics: {
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
+    umamiAnalytics: {
+      // We use an env variable for this site to avoid other users cloning our analytics ID
+      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+    },
+    // plausibleAnalytics: {
+    //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    // },
+    // simpleAnalytics: {},
+    // posthogAnalytics: {
+    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    // },
+    // googleAnalytics: {
+    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
+    // },
+  },
 
   // 뉴스레터 설정
   // newsletter: {
